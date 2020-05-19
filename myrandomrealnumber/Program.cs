146d26089e4e46eea842520ec6cc26d8b0f1f87f
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-05-20 05:00:13 +0900

    05번 문제

--- myrandomrealnumber/Program.cs
+++ myrandomrealnumber/Program.cs
@@ -10,6 +10,31 @@ namespace myrandomrealnumber
     {
         static void Main(string[] args)
         {
+            Random r = new Random();
+
+            int ran = r.Next(1, 100);
+            int num = 0;
+
+            while(num != ran)
+            {
+                Console.Write("숫자를 입력해주세요 : ");
+                num = int.Parse(Console.ReadLine());
+                if (num == ran)
+                {
+                    break;
+                }
+                else if (num > ran)
+                {
+                    Console.WriteLine("\n" + num + "보다는 작은 숫자입니다.");
+                }
+                else if( num < ran)
+                {
+                    Console.WriteLine("\n" + num + "보다는 큰 숫자입니다.");
+                }
+            }
+
+            Console.WriteLine("정답입니다!");
+
         }
     }
 }
